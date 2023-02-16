e03ec657bc2099e5af15b042240a8fcd246b08db
Eli Ravitz <eliravi1232@gmail.com>
2023-02-16 10:00:42 +0200

    02

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const baseURL = ''
 // const baseURL = 'https://find-the-treasure.onrender.com'
-const baseURL = 'https://treasure-hunting.herokuapp.com'
+const baseURL = 'https://find-the-treasure-backend.herokuapp.com/'
 const axiosInstance = axios.create({
   withCredentials: true,
   baseURL,
