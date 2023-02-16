fa389a3d95069bd0d25b60ea6803d4e2f369f22e
Eli Ravitz <eliravi1232@gmail.com>
2023-02-16 14:54:34 +0200

    08

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const baseURL = ''
-process.env.NODE_ENV === 'production'
-  ? 'https://find-the-treasure-backend.herokuapp.com'
-  : ''
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://find-the-treasure-backend.herokuapp.com'
+    : ''
 console.log('baseURL', baseURL)
 const axiosInstance = axios.create({
   withCredentials: true,
