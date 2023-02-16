af652da3385a98ca836ec7a4c9e1cc4370fda8de
Eli Ravitz <eliravi1232@gmail.com>
2023-02-16 14:52:13 +0200

    07

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ const baseURL = ''
 process.env.NODE_ENV === 'production'
   ? 'https://find-the-treasure-backend.herokuapp.com'
   : ''
-
+console.log('baseURL', baseURL)
 const axiosInstance = axios.create({
   withCredentials: true,
   baseURL,
