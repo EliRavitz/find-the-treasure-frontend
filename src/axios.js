a365727ae800dd719062de7d52adc4e8b831ce37
Eli Ravitz <eliravi1232@gmail.com>
2023-02-18 21:00:56 +0200

    01

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,12 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://lazy-erin-cormorant-sari.cyclic.app'
+    ? 'https://find-the-treasure-backend.herokuapp.com'
     : ''
+// const baseURL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://lazy-erin-cormorant-sari.cyclic.app'
+//     : ''
 
 const axiosInstance = axios.create({
   withCredentials: true,
