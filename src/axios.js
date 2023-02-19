3027efa396fa40d3a90f84d3a70f199442d37014
Eli Ravitz <eliravi1232@gmail.com>
2023-02-19 11:30:37 +0200

    05

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://azure-squid-yoke.cyclic.app'
-    : ''
 // const baseURL =
 //   process.env.NODE_ENV === 'production'
-//     ? 'https://find-the-treasure-backend.herokuapp.com'
+//     ? 'https://azure-squid-yoke.cyclic.app'
 //     : ''
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://find-the-treasure-backend.herokuapp.com'
+    : ''
 
 const axiosInstance = axios.create({
   withCredentials: true,
